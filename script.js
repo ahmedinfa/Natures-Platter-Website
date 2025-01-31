1fafc23e02698086bed596ef7f1ef17489eb48ce
ahmedinfa <info.infa02@gmail.com>
2025-02-01 02:46:51 +0600

    add arrival & challenge/slider section

---
+++ script.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: "auto",
+  // spaceBetween: 10,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+});
